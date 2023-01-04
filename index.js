ab4080ef3086a26c3165c3b89d76803f07f2c96e
Jana Rangasamy <janagr7@gmail.com>
2023-01-04 11:29:38 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,18 +6,15 @@ const arrayHasValue = require('array-has-value');
  * 
  * @param {array} sourceArray | required |
  * @param {object} opts | optional | controls the uniqueness of the array based on the value of caseSensitive and strictType
- * @returns {array} deduplicated array
- * 
+ * @returns {array} deduplicated array 
  */
 const deduplicateArray = (sourceArray, opts) => {
-
   if (!sourceArray) {
     throw new Error('sourceArray is missing.')
   }
   if (!Array.isArray(sourceArray)) {
     throw new Error('sourceArray must be an array.');
   }
-
   let deduplicatedArray = [];
   for (let item of sourceArray) {
     const isPresent = opts ? arrayHasValue(deduplicatedArray, item, opts) : arrayHasValue(deduplicatedArray, item);
